e37290a7eb2b8afa3403aa477a6e88e5a6de4267
アクゼスティア <akzestia@gmail.com>
2025-12-20 09:03:55 +0900

    Update ls.lua

--- lua/cql/ls.lua
+++ lua/cql/ls.lua
@@ -3,21 +3,22 @@ local M = {}
 function M.setup()
 	local lspconfig = require("lspconfig")
 	local configs = require("lspconfig.configs")
+	local util = require("lspconfig.util")
 
-	if not configs.cql then
-		configs.cql = {
-			default_config = {
-				cmd = { "cql_lsp" },
-				filetypes = { "cqlang" },
-				root_dir = function(fname)
-					return util.find_git_ancestor(fname) or util.path.dirname(fname)
-				end,
-				capabilities = require("blink.cmp").get_lsp_capabilities(),
-			},
-		}
-	end
+if not configs.cql then
+    configs.cql = {
+        default_config = {
+            cmd = { "cql_lsp" },
+            filetypes = { "cqlang" },
+            root_dir = function(fname)
+                return util.find_git_ancestor(fname) or util.path.dirname(fname)
+            end,
+            capabilities = require("blink.cmp").get_lsp_capabilities(),
+        },
+    }
+end
 
-	lspconfig.cql.setup({})
+lspconfig.cql.setup({})
 end
 
 return M
