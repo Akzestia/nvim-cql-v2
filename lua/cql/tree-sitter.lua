4349ad249c87a0d7bc7308e0d4788744ed49b00b
アクゼスティア <akzestia@gmail.com>
2026-04-17 22:54:35 +0900

    Update tree-sitter.lua

--- lua/cql/tree-sitter.lua
+++ lua/cql/tree-sitter.lua
@@ -1,37 +1,36 @@
 local M = {}
 
 function M.setup()
-    vim.filetype.add({
-        extension = { cql = "cqlang" },
-    })
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "TSUpdate",
+		callback = function()
+			require("nvim-treesitter.parsers").cql = {
+				install_info = {
+					url = "https://github.com/Akzestia/tree-sitter-cql",
+					revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
+					queries = "queries/neovim",
+				},
+			}
+		end,
+	})
 
-    vim.treesitter.language.register("cql", "cqlang")
+	vim.filetype.add({
+		extension = {
+			cql = "cqlang",
+		},
+	})
 
-    vim.schedule(function()
-        local ok, parsers = pcall(require, "nvim-treesitter.parsers")
-        if ok then
-            local cfg = type(parsers.get_parser_configs) == "function"
-                and parsers.get_parser_configs()
-                or parsers
+	vim.treesitter.language.register("cql", "cqlang")
 
-            cfg.cql = {
-                install_info = {
-                    url = "https://github.com/Akzestia/tree-sitter-cql",
-                    files = { "src/parser.c" },
-                    revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
-                    queries = "queries/neovim",
-                },
-                filetype = "cqlang",
-            }
-        end
-    end)
-
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = "cqlang",
-        callback = function()
-            vim.treesitter.start()
-        end,
-    })
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "cqlang",
+		callback = function(args)
+			local started, err = pcall(vim.treesitter.start, args.buf, "cql")
+			if not started then
+				vim.notify("CQL: " .. tostring(err), vim.log.levels.WARN)
+			end
+		end,
+	})
 end
 
 return M
