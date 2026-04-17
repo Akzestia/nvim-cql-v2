e2a9f7e7eb91b2df83a3e96deb6320ba91e78e49
アクゼスティア <akzestia@gmail.com>
2026-04-17 22:05:27 +0900

    Update tree-sitter.lua

--- lua/cql/tree-sitter.lua
+++ lua/cql/tree-sitter.lua
@@ -1,24 +1,21 @@
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "TSUpdate",
-		callback = function()
-			require("nvim-treesitter.parsers").cql = {
-				install_info = {
-					url = "https://github.com/Akzestia/tree-sitter-cql",
-					revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
-					queries = "queries/neovim",
-				},
-			}
-		end,
-	})
+	require("nvim-treesitter.parsers").get_parser_configs().cql = {
+		install_info = {
+			url = "https://github.com/Akzestia/tree-sitter-cql",
+			revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
+			queries = "queries/neovim",
+		},
+		filetype = "cqlang",
+	}
 
 	vim.filetype.add({
 		extension = {
 			cql = "cqlang",
 		},
 	})
+
 	vim.treesitter.language.register("cql", "cqlang")
 
 	vim.api.nvim_create_autocmd("FileType", {
